274e828d7389888aa717ae09fe99c451a9e57fc3
Mohammed Eid <meid5170@gmail.com>
2026-05-24 21:16:57 +0300

    first upload

---
+++ js/script.js
@@ -0,0 +1,102 @@
+function generateMessage() {
+
+    const name =
+    document.getElementById("personName").value;
+
+    const message =
+    document.getElementById("messageInput").value;
+
+    if(name.trim() === "" || message.trim() === ""){
+        alert("اكتب الاسم والرسالة 😌");
+        return;
+    }
+
+    const url =
+    `${window.location.origin}${window.location.pathname}` +
+    `?name=${encodeURIComponent(name)}` +
+    `&msg=${encodeURIComponent(message)}`;
+
+    navigator.clipboard.writeText(url);
+
+    alert("تم نسخ الرابط 😈 ابعته على واتساب");
+}
+
+
+function showMessage(name, message){
+
+    document
+        .getElementById("setup")
+        .classList.add("hidden");
+
+    document
+        .getElementById("messageScreen")
+        .classList.remove("hidden");
+
+    document
+        .getElementById("title")
+        .innerHTML =
+        `✨ رسالة إلى ${name} ✨`;
+
+    const typing =
+    document.getElementById("typing");
+
+    typing.innerHTML = "";
+
+    let i = 0;
+
+    const effect = setInterval(()=>{
+
+        typing.innerHTML +=
+        message[i] === "\n"
+        ? "<br>"
+        : message[i];
+
+        i++;
+
+        if(i >= message.length){
+            clearInterval(effect);
+        }
+
+    },40);
+}
+
+
+window.onload = () => {
+
+    const params =
+    new URLSearchParams(window.location.search);
+
+    const name = params.get("name");
+    const msg = params.get("msg");
+
+    if(name && msg){
+        showMessage(name, msg);
+    }
+}
+
+
+function createHeart(){
+
+    const heart =
+    document.createElement("div");
+
+    heart.innerHTML = "❤️";
+    heart.classList.add("heart");
+
+    heart.style.left =
+    Math.random()*100 + "vw";
+
+    heart.style.fontSize =
+    (15 + Math.random()*25) + "px";
+
+    heart.style.animationDuration =
+    (4 + Math.random()*3) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },7000);
+}
+
+setInterval(createHeart,300);
